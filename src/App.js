887be4a0ba0c4fe7604c00bd7a0bbf116bdbc61d
Tim Koopmans <tim.koops@gmail.com>
2023-08-22 19:40:16 +1000

    tidy up

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
 import './App.css';
-import { useAuth } from "@frontegg/react";
+import {useAuth} from "@frontegg/react";
 
 function App() {
-  const { user, isAuthenticated } = useAuth();
+    const {user, isAuthenticated} = useAuth();
 
-  const redirectToLogin = () => {
-      window.location.href = `https://auth.correkt.horse?redirectUrl=${window.location}`;
-  }
+    const logout = () => {
+        window.location.href = `${window.location}account/logout`;
+    }
 
-  return (
-      <div className='App'>
-        {isAuthenticated ? (
-            <div>
-              <img src={user.profilePictureUrl} alt={user.name} />
-              <span>{user.name}</span>
-            </div>
+    const redirectToLogin = () => {
+        window.location.href = `https://auth.correkt.horse?redirectUrl=${window.location}`;
+    }
 
-        ) :
-            <div>
+    return (<div className='App'>
+            {isAuthenticated ? (<div>
+                    <h1>app1.correkt.horse</h1>
+                    <div>
+                        <img src={user?.profilePictureUrl} alt={user?.name} />
+                        <span>{user?.name}</span>
+                    </div>
+                    <div>
+                        <button onClick={() => logout()}>Logout</button>
+                    </div>
+                </div>
+
+            ) : <div>
                 <h1>app1.correkt.horse</h1>
-                <button onClick={() => redirectToLogin()}>Click to authenticate</button>
-            </div>
-        }
-      </div>
-  );
+                <button onClick={() => redirectToLogin()}>Login</button>
+            </div>}
+        </div>);
 }
 
 export default App;
